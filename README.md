4b1016689bb1878907b1ebce65f1967701cbb39d
gruppi <mauricio.gruppi@gmail.com>
2020-03-16 12:28:58 -0400

    Update scripts

--- README.md
+++ README.md
@@ -4,8 +4,13 @@ This repository contain examples of how to use the NELA-GT-2019 data set with Py
 
 ## load-sqlite3.py
 
-Shows how to load the data from the Sqlite3 database using SQL queries.
+* How to load the data from the Sqlite3 database using SQL queries.
+  + Loading data from single or multiple sources from the database
+  + Loading data from the database into a Pandas dataframe
+
 
 ## load-json.py
 
-Shows how to load source JSON files.
+* How to load NELA in JSON format with Python 3.
+  + Loading a single source's JSON
+  + Loading a directory of NELA JSON files - **WARNING**: this may take lots of memory
---
+++ load-sqlite3.py
@@ -0,0 +1,61 @@
+import argparse
+import sqlite3
+import pandas as pd
+
+# This script shows an example of how to use NELA-GT-2019 with sqlite3
+
+# Execute a given SQL query on the database and return values
+def execute_query(path, query):
+    conn = sqlite3.connect(path)
+    # execute query on database and retrieve them with fetchall
+    results = conn.cursor().execute(query).fetchall()
+    return results
+
+
+# Execute query and load results into pandas dataframe
+def execute_query_pandas(path, query):
+    conn = sqlite3.connect(path)
+    df = pd.read_sql_query(query, conn)
+    return df
+
+
+# Start here
+def main():
+    # Make input command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path", type=str, help="Path to database file")
+    args = parser.parse_args()
+
+    # Query 1: select all articles from a specific source
+    source = "thenewyorktimes"
+    query = "SELECT * FROM newsdata WHERE source='%s'" % source
+
+    data = execute_query(args.path, query)
+
+    print("-> Found %d articles from %s" % (len(data), source))
+
+    # Query 2: select articles from multiple sources
+    sources = ['thenewyorktimes', 'cnn', 'foxnews']
+    # Note that we need to add extra quotes around each source's name
+    # for the query to work properly e.g.: "'thenewyorktimes'"
+    sources_str = ["'%s'" % s for s in sources]
+    query = "SELECT * FROM newsdata WHERE source IN (%s)" % ",".join(sources_str)
+    data = execute_query(args.path, query)
+
+    print("-> Found %d articles from %s" % (len(data), ",".join(sources)))
+
+    # Alternatively, one can fetch queries into a pandas dataframe:
+    df = execute_query_pandas(args.path, query)
+
+    print("-- Same results but in a Pandas dataframe now")
+    print(df)
+
+    print("- ALL DONE.")
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
